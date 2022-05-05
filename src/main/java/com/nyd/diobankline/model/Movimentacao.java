28d98169d9aa8002016aa612ed60c9ab072b91ea
Renato Avellar <54222065+DonRenato@users.noreply.github.com>
2022-05-05 18:44:25 -0300

    ajustando formatação da hora

--- src/main/java/com/nyd/diobankline/model/Movimentacao.java
+++ src/main/java/com/nyd/diobankline/model/Movimentacao.java
@@ -1,5 +1,7 @@
 package com.nyd.diobankline.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
@@ -10,6 +12,7 @@ public class Movimentacao {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "dt_hora")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dataHora;
     private String descricao;
     private Double valor;
